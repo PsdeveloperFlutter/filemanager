b731d5bb0811aa8a11b1c9a4a4f790b54490e829
Priyanshu Satija <Nevergiveupjsr@gmail.com>
2025-07-31 16:55:47 +0530

    Refactor: Update long-press behavior in file manager
    This commit modifies the `onLongPress` behavior for items in the file manager screen:
    
    - **`filemanagerScreen.dart`**:
        - The `onLongPress` callback will now only trigger selection mode if `selectedItems` is not empty.
        - If `selectedItems` is empty, long-pressing an item will now add it to `selectedItems` without immediately enabling selection mode.

--- lib/FileManagement/mainFile/filemanagerScreen.dart
+++ lib/FileManagement/mainFile/filemanagerScreen.dart
@@ -284,7 +284,7 @@ class FileManagerScreenSubState extends State<FileManagerScreenSub> {
                       OpenFilex.open(item.path); // ✅ Open file on tap
                     }
                   },
-                  onLongPress: !isSelectionMode && !isFolder
+                  onLongPress: !isSelectionMode && !isFolder  && selectedItems.isNotEmpty
                       ? () {
                     setState(() {
                       isSelectionMode = true;
@@ -293,7 +293,10 @@ class FileManagerScreenSubState extends State<FileManagerScreenSub> {
                       }
                     });
                   }
-                      : null,
+                      : (){
+                    selectedItems.add(item);
+                    null;
+                  },
                   leading: isSelectionMode && !isFolder
                       ? Checkbox(
                     value: isSelected,
